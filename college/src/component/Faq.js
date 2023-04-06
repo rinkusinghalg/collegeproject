b4501ffb021554100b37cef1f531c67430af1605
Rinku singh <rinkusinghalg@gmail.com>
2023-04-06 17:31:13 +0530

    some change

--- college/src/component/Faq.js
+++ college/src/component/Faq.js
@@ -4,7 +4,7 @@ function Faq(){
             <h1>contact us with college</h1>
             <span style={{color:"red",fontSize:"30px"}}>college:</span><span style={{color:"blue",fontSize:"30px"}}>8395042915</span><br></br>
             <span style={{color:"pink",fontSize:"30px"}}>email:</span><span style={{color:"yellow",fontSize:"30px"}}>acet@gmail.com</span>
-            <span style={{color:"pink",fontSize:"30px"}}>email:</span><span style={{color:"yellow",fontSize:"30px"}}>acet@gmail.com</span>
+            <span style={{color:"pink",fontSize:"35px"}}>email:</span><span style={{color:"yellow",fontSize:"30px"}}>acet@gmail.com</span>
         </div>
     )
 }export default Faq;
